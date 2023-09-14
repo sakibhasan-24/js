c4c02a6685be805c34376cad84053859f11a0c5b
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-09-14 22:12:17 +0600

    js-fun

--- script.js
+++ script.js
@@ -2,3 +2,97 @@
 console.log("be stronger in Fundamental!");
 
 // data types
+
+/*
+=>primitive data types
+=>non-primitive data types(array,objects)
+*/
+
+// const userName = "sakib hasan"; //string data types
+// const userAge = 29; //Number data types
+// const isStudent = false; //boolean type
+// let amount;
+// let yearRemaining = null;
+// const gradutaionYear; undefined
+// console.log(typeof isStudent);
+// console.log(typeof amount);
+// console.log(typeof yearRemaining);
+
+// one practice problem
+
+/**
+ * there are two user
+ * one used iPhone+ a smart watch
+ * one used laptop+ a note book
+ * there total price and comapre who is higher
+ *
+ */
+
+// task---1
+/*
+const userOneIphone = 1200;
+const userOneSmartWatch = 530;
+const userOneTotal = userOneIphone + userOneSmartWatch;
+
+const userTwoILaptop = 900;
+const userTwoNoteBook = 330;
+const userTwoTotal = userTwoILaptop + userTwoNoteBook;
+console.log(userOneTotal, userTwoTotal);
+const isHigher = userOneTotal > userTwoTotal;
+console.log(isHigher);
+console.log("user One Total is higher than User Two= ", isHigher);
+*/
+
+/*
+const userName = "sakib ";
+const userAge = 29;
+console.log(
+  "hey! this is " + userName + " who is still jobless when he is " + userAge
+);
+console.log(
+  `hey! this is ${userName} who is still jobless when he is ${userAge + 1}`
+);
+if (userAge <= 30) {
+  console.log(`still ${30 - userAge} year's is remaining`);
+} else {
+  console.log(`out of age`);
+}*/
+// type conversion vs type coersion
+// conversion
+/*
+const ourTargetvalue = "5";
+console.log(typeof ourTargetvalue);
+console.log(typeof Number(ourTargetvalue));
+// Coercion is an automatic type conversion
+// Most famous string and number coersion
+console.log("10" + 10);
+console.log("3" == 3);
+console.log("10" - 2); //weired behaviour
+console.log(typeof (10 - "2" + 2 + "3")); //string
+console.log(typeof (10 - "2" + 2)); //number
+*/
+
+/*
+const allowAge = 22;
+const msg = allowAge >= 18 && "userAllow";
+const msgForConsideration =
+  (allowAge >= 18 || allowAge + 1 >= 16) && "user Allow After One Year";
+console.log(msg);
+console.log(msgForConsideration);
+*/
+
+// truthy and falsy value
+// console.log("5 Falsy Value");
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(""));
+
+// // a very stupid behaviour
+// const nums = 0;
+// if (nums) {
+//   console.log("you started from here !");
+// } else {
+//   console.log("stupid guys");
+// }
